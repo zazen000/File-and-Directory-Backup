18e4f851aec667a357b020f15dcbc0b47ae2f435
zazen000 <49261045+zazen000@users.noreply.github.com>
2021-01-21 11:48:41 -0800

    Update qtpy_cfg.py

--- qtpy_cfg.py
+++ qtpy_cfg.py
@@ -26,14 +26,18 @@ def qbutton(self):
 def qbutton_dim(self):
     return "QPushButton {color: rgba(54, 136, 200, 250); background-color: black; }"
 
+
 def qbutton_red(self):
     return "QPushButton {color: rgba(236, 28, 36, 200); background-color: rgba(20, 21, 29, 150);}"
+
 
 def qbutton_green(self):
     return "QPushButton {color: rgba(6, 186, 39, 250); background-color: rgba(20, 21, 29, 150);}"
 
+
 def qbutton_blue(self):
     return "QPushButton {color: rgba(54, 136, 200, 250); background-color: rgba(20, 21, 29, 150);}"
+
 
 def qlabel(self):
     return "QLabel {color: rgba(80,130,255,255); background-color: black;}"
@@ -46,18 +50,11 @@ def qledit(self):
     return "QLineEdit {background-color: black; font: normal; color: rgba(162,201,229,255); border: black;}"
 
 
-def menu_timer(self: object,time: object) -> object:
+def menu_timer(self: object, time: object) -> object:
     self.thread = PyQt5.QtCore.QThread()
     self.thread.start()
     self.timer.start(time)
     self.thread.exit()
-
-
-def qtframe(self, name, x,y,w,h):
-#    self.name = QFrame(self)
-    self.name.setFrameShape(QFrame.NoFrame)
-    self.name.setGeometry(QRect(x, y, w, h))
-    self.name.setStyleSheet('background-color:black')
 
 
 def qtbutton(self, name, x, y, w, h, style, font, title, connection):
@@ -79,13 +76,4 @@ def qtlabel(self, name, x, y, w, h, style, font, align,):
 def qttimer(self, name, connection):
     self.name.timeout.connect(self.connection)
     self.connection()
-
-
-def db(database_name, num=1):
-    client = MongoClient('localhost',27017)
-    db = client[database_name]
-    coll = col+num
-    for n in num:
-        coll = db[coll]
-    return
 
