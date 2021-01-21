3570d97e07973a2900ef2487617254fb17638365
zazen000 <49261045+zazen000@users.noreply.github.com>
2021-01-20 19:32:50 -0800

    Update backup-hq.py

--- backup-hq.py
+++ backup-hq.py
@@ -1,7 +1,5 @@
 import os
 
-
-os.system(r'start xcopy C:\"Program Files"\MongoDB\Server\4.2 C:\data\db /d/y')
 os.system(r"start /min python backup.py")
 os.system(r"start /min python backup_main.py")
 
